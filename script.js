dd026001f9c7735f2568ca05d172c47c0e7b6a74
Abz1717 <2457570@dundee.ac.uk>
2024-04-08 21:33:04 +0100

    added functionality to rock paper scissors through buttons

--- script.js
+++ script.js
@@ -31,28 +31,74 @@ function playRound(playerSelection, computerSelection){
 
     if (playerSelection == computerSelection){
         roundWinner = "Tie"
-    }
-  
+    } 
 }
 
 
 function isGameOver(){
 
+    return (playerPoints + computerPoints >= rounds);
 }
 
 function updateCurrentScore(){
 
+    const scoreDisplay = document.getElementById('current-score');
+    scoreDisplay.innerText = `Score: Player ${playerPoints} - Computer ${computerPoints}`;
 }
 
 function updateFinalScore(){
 
+    let finalMessage;
+    if (playerPoints > computerPoints) {
+        finalMessage = `Game Over! You won ${playerPoints} to ${computerPoints}.`;
+    } else if (computerPoints > playerPoints) {
+        finalMessage = `Game Over! Computer won ${computerPoints} to ${playerPoints}.`;
+    } else {
+        finalMessage = `Game Over! It's a tie: ${playerPoints} to ${computerPoints}.`;
+    }
+    alert(finalMessage);
+}
+
+function restartGame(){
+    playerPoints = 0
+    computerPoints = 0 
+}
+
+function highlightComputerChoice(computerSelection) {
+
+     document.querySelectorAll('#computer-choices button').forEach(button => {
+        button.classList.remove('highlight');
+    });
+
+    const buttonId = `computer-${computerSelection.toLowerCase()}`;
+    const selectedButton = document.getElementById(buttonId);
+    
+    if (selectedButton) {
+        selectedButton.classList.add('highlight');
+    } else {
+        console.log(`Button with ID ${buttonId} not found.`);
+    }
 }
 
 
 
+document.addEventListener("click", gameSelectionListener);
+
+function gameSelectionListener(event){
+
+    if (event.target.matches("#player-choices button")) { 
+        const playerSelection = event.target.id.replace('player-', '').toUpperCase();
+        playerClick(playerSelection);
+        if (isGameOver()) {
+            updateFinalScore();
+        }
+    }
+}
 
 function playerClick(playerSelection){
 
     const computerSelection = getComputerChoice();
-
+    playRound(playerSelection, computerSelection);
+    highlightComputerChoice(computerSelection);
+    updateCurrentScore();
 }
